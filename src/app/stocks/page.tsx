7d22318d56d240206cce5ed80c53c80072cd9b2b
scott0229 <scott.sh.lee@gmail.com>
2026-04-22 15:06:30 +0800

    Rename column to 開倉後總股數

--- src/app/stocks/page.tsx
+++ src/app/stocks/page.tsx
@@ -409,7 +409,7 @@ export default function StockTradingPage() {
                                 <TableHead className="text-center">平倉價</TableHead>
                                 <TableHead className="text-center">當前股價</TableHead>
                                 <TableHead className="text-center">盈虧</TableHead>
-                                <TableHead className="text-center">當前累積股數</TableHead>
+                                <TableHead className="text-center">開倉後總股數</TableHead>
                                 <TableHead className="text-center">列入期權</TableHead>
                                 {settings.showTradeCode && <TableHead className="text-center">交易代碼</TableHead>}
                                 <TableHead className="text-right"></TableHead>
